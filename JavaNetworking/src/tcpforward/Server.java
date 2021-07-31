cd84c3906b7b2148295ad5d62a3ab5311a27b77d
Asif-droid <58304463+Asif-droid@users.noreply.github.com>
2021-07-31 23:09:50 +0600

    Add files via upload

---
+++ JavaNetworking/src/tcpforward/Server.java
@@ -0,0 +1,37 @@
+package tcpforward;
+
+import util.NetworkUtil;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    public HashMap<String, NetworkUtil> clientMap;
+
+    Server() {
+        clientMap = new HashMap<>();
+        try {
+            serverSocket = new ServerSocket(44444);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                serve(clientSocket);
+            }
+        } catch (Exception e) {
+            System.out.println("Server starts:" + e);
+        }
+    }
+
+    public void serve(Socket clientSocket) {
+        NetworkUtil networkUtil = new NetworkUtil(clientSocket);
+        String clientName = (String) networkUtil.read();
+        clientMap.put(clientName, networkUtil);
+        new ReadThreadServer(clientMap, networkUtil);
+    }
+
+    public static void main(String args[]) {
+        Server server = new Server();
+    }
+}
---
+++ JavaNetworking/src/tcpstring/ReadThread.java
@@ -0,0 +1,32 @@
+package tcpstring;
+
+import util.NetworkUtil;
+
+public class ReadThread implements Runnable {
+    private Thread thr;
+    private NetworkUtil networkUtil;
+
+    public ReadThread(NetworkUtil networkUtil) {
+        this.networkUtil = networkUtil;
+        this.thr = new Thread(this);
+        thr.start();
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                String s = (String) networkUtil.read();
+                if (s != null) {
+                    System.out.println("Received: " + s);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            networkUtil.closeConnection();
+        }
+    }
+}
+
+
+
